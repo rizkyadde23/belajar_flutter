2e0e8302a8ff4ebcec86e9a9d40a45c89279ed86
rizkyadde23 <rizkyadde23@gmail.com>
2025-06-21 20:25:19 +0700

    Input Output v2 App

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,108 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Stateful Widget',
+      title: "StateFull Widget",
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(),
+      home: MahasiswaListPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
+class MahasiswaListPage extends StatefulWidget{
+  const MahasiswaListPage({super.key});
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MahasiswaListPage> createState() => _MahasiswaListPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MahasiswaListPageState extends State<MahasiswaListPage>{
   final TextEditingController namaController = TextEditingController();
-  final TextEditingController nimController  = TextEditingController();
+  final TextEditingController nimController = TextEditingController();
+  List<String> namaMahasiswa = [];
+  List<String> nimMahasiswa = [];
 
-  String nama = '';
-  String nim  = '';
-
-  void tampilkan() {
-    setState(() {
-      nama = namaController.text;
-      nim  = nimController.text;
-    });
+  void tambahData(){
+    if (namaController.text.isNotEmpty){
+      setState(() {
+        namaMahasiswa.add(namaController.text,);
+        nimMahasiswa.add(nimController.text);
+        namaController.clear();
+        nimController.clear();
+      });
+    }
   }
 
-  void reset() {
+  void deleteData(){    
     setState(() {
-      namaController.clear();
-      nimController.clear();
-      nama = '';
-      nim  = '';
+      namaMahasiswa.clear();
+      nimMahasiswa.clear();
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Input And Output')),
+      appBar: AppBar(title: Text('ADD AND DELETE')),
       body: Padding(
-        padding: const EdgeInsets.all(16),
+        padding: EdgeInsets.all(16),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: namaController,
-              decoration: const InputDecoration(
-                labelText: 'Masukkan Nama Anda',
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextField(
+            controller: namaController,
+            decoration: const InputDecoration(
+              labelText: "Masukkan Nama"
+            ),
+          ),
+          const SizedBox(height: 12),
+          TextField(
+            controller: nimController,
+            decoration: InputDecoration(
+              labelText: "Masukkan NIM"
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: tambahData,
+                child: const Text("Tambah Data")
               ),
-            ),
-            const SizedBox(height: 12),
-            TextField(
-              controller: nimController,
-              decoration: const InputDecoration(
-                labelText: 'Masukkan NIM Anda',
+              ElevatedButton(
+                onPressed: deleteData, 
+                child: const Text("Hapus Data")
               ),
-              keyboardType: TextInputType.number,
-            ),
-            const SizedBox(height: 20),
-            // --- Tombol ---
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: tampilkan,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.greenAccent,
-                  ), 
-                  child: const Text('Tampilkan')),
-                ElevatedButton(
-                  onPressed: reset,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red,
-                  ),
-                  child: const Text('Reset'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 30),
-            // --- Hasil ---
-            if (nama.isNotEmpty && nim.isNotEmpty) ...[
-              Text('Selamat Datang $nama di Flutter', style: const TextStyle(fontSize: 16)),
-              Text('NIM Anda adalah $nim', style: const TextStyle(fontSize: 16)),
             ],
-          ],
-        ),
-      ),
+          ),
+          const SizedBox(height: 20),
+          Text("Daftar Mahasiswa :",
+          style: TextStyle(
+            fontSize: 16
+          ),
+          ),
+          const SizedBox(height: 14),
+          Expanded(
+            child:ListView.builder(
+                  itemCount: namaMahasiswa.length,
+                  itemBuilder: (context, index){
+                    return ListTile(
+                      leading: const Icon(Icons.person),
+                      title: Text('${namaMahasiswa[index]} (${nimMahasiswa[index]})'),
+                    );
+                  }
+                  ),
+            )
+          ]
+          )
+        ,
+      ),    
     );
-  }
+}
 }
