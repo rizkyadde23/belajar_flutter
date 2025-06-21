fa83f8386ea5e32a7b0e9a6e8ebf1526a77be328
rizkyadde23 <rizkyadde23@gmail.com>
2025-06-21 20:13:02 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "StateFull Wdget",
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String teks = 'Increment';
+  int i = 0;
+  void increment() {
+    setState(() {
+      i++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Stateful Widget')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              teks,
+              style: TextStyle(fontSize: 28),
+            ),
+            Text(
+              i.toString(),
+              style: TextStyle(fontSize: 28),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: increment,
+              child: Text('Tambah Angka'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
