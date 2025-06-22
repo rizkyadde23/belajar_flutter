629fbe77d01cd6b0fe8cea39fa8303e87d5a4e6a
rizkyadde23 <rizkyadde23@gmail.com>
2025-06-22 19:48:28 +0700

    LocalStorage App

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,55 +1,101 @@
 import 'package:flutter/material.dart';
-import 'detail.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+class LocalStoragePage extends StatefulWidget{
+  const LocalStoragePage({super.key});
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<LocalStoragePage> createState() => _LocalStoragePageState();
 }
 
-class _HomePageState extends State<HomePage>{
-  final TextEditingController namaController = TextEditingController();
-  final TextEditingController nimController = TextEditingController();
+class _LocalStoragePageState extends State<LocalStoragePage>{
+final TextEditingController namaController = TextEditingController();
+final TextEditingController nimController = TextEditingController();
 
-  void openDetail(){
-    if(namaController.text.isNotEmpty && nimController.text.isNotEmpty){
-      Navigator.push(context, MaterialPageRoute(builder: (context) => DetailPage(
-        nama : namaController.text,
-        nim : nimController.text
-        )
-      )
-      );
-    }
-  }
-    @override
-    Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(title: Text("NAVIGATOR"),),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextField(
-              controller: namaController,
-              decoration: InputDecoration(
-              labelText: "Masukkan Nama"
-              ),
-            ),
-            const SizedBox(height: 12,),
-            TextField(
-              controller: nimController,
-              decoration: InputDecoration(
-                labelText: "Masukkan NIM"
-              ),
-            ),
-            const SizedBox(height: 12,),
-            ElevatedButton(onPressed: openDetail, 
-            child: const Text("Open Detail"))
-          ],
-        ),
-        ),
-    );
-  }
+String? savedNama;
+String? savedNim;
+
+@override
+void initState(){
+  super.initState();
+  loadData();
 }
 
+Future<void> loadData() async {
+  final prefs = await SharedPreferences.getInstance();
+  setState(() {
+    savedNama = prefs.getString('nama');
+    savedNim = prefs.getString('nim');
+  });
+}
+
+Future<void> simpanData() async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.setString('nama', namaController.text);
+  await prefs.setString('nim', nimController.text);
+  loadData();
+}
+
+Future<void> hapusData() async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.remove('nama');
+  await prefs.remove('nim');
+  setState(() {
+    savedNama = null;
+    savedNim = null;
+  });
+}
+
+@override
+void dispose() {
+  namaController.dispose();
+  nimController.dispose();
+  super.dispose();
+}
+
+@override
+Widget build(BuildContext context){
+  return Scaffold(
+    appBar: AppBar(title: Text("Shared Preference"),
+    ),
+    body: Padding(padding: EdgeInsets.all(16),
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      TextField(
+        controller: namaController,
+        decoration: InputDecoration(
+          labelText: "Masukkan Nama"
+        ),
+      ),
+      const SizedBox(height: 12,),
+      TextField(
+        controller: nimController,
+        decoration: InputDecoration(
+          labelText: "Masukkan NIM"
+        ),
+      ),
+      const SizedBox(height: 12,),
+      Row(
+        children: [
+          ElevatedButton(onPressed: simpanData,
+           style: ElevatedButton.styleFrom(backgroundColor: Colors.greenAccent),
+           child: Text("Simpan")),
+          ElevatedButton(onPressed: hapusData, 
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.redAccent),
+          child:Text("Hapus"),
+          )
+        ],
+      ),
+      const SizedBox(height: 12,),
+      Text("Data Tersimpan", style: TextStyle(fontSize:16),),
+      const SizedBox(height: 12,),
+      Text('Nama : ${savedNama ?? "-"}'),
+      Text('NIM : ${savedNim ?? "-"}'),
+    ],
+    ),
+    ),
+  );
+}
+
+}
