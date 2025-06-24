7b7087fc6bdfb86d720545c2fdc0621029b2eff4
rizkyadde23 <rizkyadde23@gmail.com>
2025-06-24 10:10:42 +0700

    ListView Builder v2 App

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
+import 'mahasiswalist.dart';
+
+class Mahasiswa {
+  final String nama;
+  final String nim;
+  Mahasiswa({required this.nama,required this.nim});  
+}
 
 class HomePage extends StatelessWidget{
   HomePage({super.key});
-  final List<Map<String,String>> daftarMahasiswa = [
-    {'nama': 'Rizky', 'nim': '123240150'},
-    {'nama': 'Zaka', 'nim': '123240157'},
-    {'nama': 'Ardi', 'nim': '123240149'},
-  ];
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,8 +23,8 @@ class HomePage extends StatelessWidget{
             margin: EdgeInsets.all(8),
             child: ListTile(
               leading: Icon(Icons.person),
-              title: Text('NAMA : ${mahasiswa['nama']}'),
-              subtitle: Text('NIM : ${mahasiswa['nim']}'),
+              title: Text('NAMA : ${mahasiswa.nama}'),
+              subtitle: Text('NIM : ${mahasiswa.nim}'),
             ),
           );
         }
