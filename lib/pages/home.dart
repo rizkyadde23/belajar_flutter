c6907ef1e0dfbac887eb1e31f81b6d3b8ed24de1
rizkyadde23 <rizkyadde23@gmail.com>
2025-06-24 10:33:03 +0700

    ListView Builder v3 App

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'mahasiswalist.dart';
 
 class Mahasiswa {
   final String nama;
@@ -7,28 +6,80 @@ class Mahasiswa {
   Mahasiswa({required this.nama,required this.nim});  
 }
 
-class HomePage extends StatelessWidget{
-  HomePage({super.key});
+class DynamicMahasiswaList extends StatefulWidget{
+  const DynamicMahasiswaList({super.key});
+
+  @override
+  State<DynamicMahasiswaList> createState() => _DynamicMahasiswaListState();
+}
+
+class _DynamicMahasiswaListState extends State<DynamicMahasiswaList>{
+  List<Mahasiswa> daftarMahasiswa = [];
+  final TextEditingController namaController = TextEditingController();
+  final TextEditingController nimController = TextEditingController();
+
+  void tambahData(){
+  if (namaController.text.isNotEmpty && nimController.text.isNotEmpty) {
+    setState(() {
+      daftarMahasiswa.add(Mahasiswa(nama: namaController.text, nim: nimController.text));
+    });
+    namaController.clear();
+    nimController.clear();
+  }    
+  }
+
+  void hapusData(int index){
+    setState(() {
+      daftarMahasiswa.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Listview Builder"),),
       body: Padding(padding: EdgeInsets.all(16),
-      child:
-        ListView.builder(
-          itemCount: daftarMahasiswa.length,
-          itemBuilder: (context, index){
-          final mahasiswa = daftarMahasiswa[index];
-          return Card(
-            margin: EdgeInsets.all(8),
-            child: ListTile(
-              leading: Icon(Icons.person),
-              title: Text('NAMA : ${mahasiswa.nama}'),
-              subtitle: Text('NIM : ${mahasiswa.nim}'),
+      child: Column(
+        children: [
+            TextField(
+              controller: namaController,
+              decoration: InputDecoration(
+                labelText: "Masukkan Nama"
+              ),
             ),
-          );
-        }
-        )
+            const SizedBox(height: 12,),
+            TextField(
+              controller: nimController,
+              decoration: InputDecoration(
+                labelText: "Masukkan Nim"
+              ),
+            ),
+            const SizedBox(height: 12,),
+            ElevatedButton(
+            onPressed: tambahData,
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.greenAccent), 
+            child: Text("Tambah") 
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: daftarMahasiswa.length,
+                itemBuilder: (context, index){
+                final mahasiswa = daftarMahasiswa[index];
+                return Card(
+                  margin: EdgeInsets.all(8),
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text('NAMA : ${mahasiswa.nama}'),
+                    subtitle: Text('NIM : ${mahasiswa.nim}'),
+                    trailing: IconButton(onPressed: () => hapusData(index), icon: Icon(Icons.delete)),
+                  ),
+                );
+              }
+              ),
+            ),
+
+        ],
+      )
       ),
       );
   }
--- lib/pages/mahasiswalist.dart
+++
@@ -1,7 +0,0 @@
-import 'home.dart';
-final List<Mahasiswa> daftarMahasiswa = [
-    Mahasiswa(nama: 'Rizky', nim: '123240150'),
-    Mahasiswa(nama: 'Zaka', nim: '123240157'),
-    Mahasiswa(nama: 'Nico', nim: '123240151'),
-    Mahasiswa(nama: 'Andi', nim: '123240156'),
-  ];
