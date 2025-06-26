cb36f8242543c7dc0cb6eb7726119393bf1c7513
rizkyadde23 <rizkyadde23@gmail.com>
2025-06-26 20:53:09 +0700

    Form Validation App

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,27 +1,66 @@
 import 'package:flutter/material.dart';
-import 'dynamic_list.dart';
 
-class TabBarApp extends StatelessWidget{
-  const TabBarApp ({super.key});
-
-@override
-  Widget build(BuildContext context) {
-    return DefaultTabController(length: 3, 
-    child:Scaffold(
-      appBar: AppBar(
-        title: Text("TabBar"),
-        bottom: TabBar(tabs: [
-          Tab(text: "Home", icon: Icon(Icons.home)),
-          Tab(text: "List", icon: Icon(Icons.list)),
-          Tab(text: "About", icon: Icon(Icons.info)),
-        ]),
-      ),
-      body: TabBarView(children: [
-        Center(child: Text("ini Home Page"),),
-        DynamicMahasiswaList(),
-        Center(child: Text("ini About Page"),),
-      ]),
-    )
-    );
+class ValidationPage extends StatefulWidget{
+  const ValidationPage ({super.key});
+  @override
+  _ValidationPageState createState() => _ValidationPageState();
 }
+
+class _ValidationPageState extends State<ValidationPage>{
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController namaController = TextEditingController();
+  final TextEditingController nimController = TextEditingController();
+
+  void _submitForm(){
+    if (formKey.currentState!.validate()) {
+      final nama = namaController.text;
+      final nim = nimController.text;
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Data Valid: $nama ($nim)')
+      )
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Form"),),
+      body: Padding(padding: EdgeInsets.all(16),
+      child: Form(
+        key: formKey,
+        child: Column(
+        children: [
+          TextFormField(
+            controller: namaController,
+            decoration: InputDecoration(labelText: "Masukkan Nama"),
+            validator: (value){
+              if (value==null || value.isEmpty) {
+                return "Nama Tidak Boleh Kosong";
+              }
+              return null;
+            },
+            ),
+            TextFormField(
+              controller: nimController,
+              decoration: InputDecoration(labelText: "Masukkan Nim"),
+              keyboardType: TextInputType.number,
+              validator: (value){
+                if(value==null || value.isEmpty){
+                  return "NIM Tidak Boleh Kosong";
+                }
+                if (!RegExp(r'^\d+$').hasMatch(value)) {
+                  return "NIM Harus Berupa Angka";
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 12,),
+            ElevatedButton(onPressed: _submitForm, child: Text("Simpan")),
+        ],
+      )
+      ),
+      ),
+    );
+  }
+
 }
